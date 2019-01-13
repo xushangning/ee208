14067506d099111b4905dbcb660aa71b342be566
Shining Xu <xushangning@sjtu.edu.cn>
2019-01-13 20:52:55 +0800

    完成后端部分
    by cjz

--- backend-search.py
+++ backend-search.py
@@ -58,12 +58,62 @@ from run_demo_server import recognize_text_from_posters
 
 app = Flask(__name__)
 
+<<<<<<< HEAD
+@app.route('/')
+def home():
+    dsl = {'query':{'match':{'name':'X战警'}}}
+    result = es.search(index='homework1',doc_type='Movie',body=dsl)
+    res = []
+    for i in result['hits']['hits']:
+        res.append(i['_source'])
+    return res
+=======
+>>>>>>> dd7243895084e750c9ccaa52cb11233a218fb4a6
 
 @app.route('/results',methods=['GET'])
 def text_Search():
     search = request.args.get('name')
     dsl1 = {'query':{'match':{'name':str(search)}}}
     result = es.search(index='homework1',doc_type='Movie',body=dsl1)
+<<<<<<< HEAD
+    res = []
+    for i in result['hits']['hits']:
+        res.append(i['_source'])
+    return res
+
+@app.route('/result0',methods=['GET'])
+def result0():
+        search = request.args.get('name')
+        dsl2 = {'query':{'term':{'name':str(search)}}}
+        result = es.search(index='homework2',doc_type='Movie',body=dsl2)
+        res = result['hits']['hits'][0]['_source']
+        return res
+
+@app.route('/result1',methods=['POST'])
+def result1():
+        search = request.files['file']
+        search.save('/result1/upload' + secure_filename(search.filename))
+        img = cv2.imread('/result1/upload' + secure_filename(search.filename))
+        img1 = np.array(img)
+        search = recognize_text_from_posters(img1)
+        dsl3 = {'query':{'match':{'name':str(search)}}}
+        result = es.search(index='homework1',doc_type='Movie',body=dsl3)
+        res = result['hits']['hits'][0]['_source']
+        return res
+
+@app.route('/result2',methods=['POST'])
+def result1():
+    search = request.files['file']
+    search.save('/result2/upload' + secure_filename(search.filename))
+    img = cv2.imread('/result2/upload' + secure_filename(search.filename))
+    img1 = np.array(img)
+    search = recognize_text_from_posters(img1)
+    dsl4 = {'query':{'match':{'time':str(search)}}}
+    result = es.search(index='homework1',doc_type='Movie',body=dsl4)
+    res = result['hits']['hits'][0]['_source']
+    return res
+
+=======
     results = []
     for i in result['hits']['hits']:
         results.append(i['_source'])
@@ -88,6 +138,7 @@ def img_Search():
         result = es.search(index='homework1',doc_type='Movie',body=dsl3)
         result2 = result['hits']['hits'][0]['_source']
         return result2
+>>>>>>> dd7243895084e750c9ccaa52cb11233a218fb4a6
 
 
 
