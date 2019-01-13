6d35dc18abb7fa2e9420d7f8f8ada6ed0a751641
Shining Xu <xushangning@sjtu.edu.cn>
2019-01-13 21:01:26 +0800

    合并 backend_search.py 版本

--- backend-search.py
+++ backend-search.py
@@ -58,7 +58,6 @@ from run_demo_server import recognize_text_from_posters
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 @app.route('/')
 def home():
     dsl = {'query':{'match':{'name':'X战警'}}}
@@ -67,15 +66,12 @@ def home():
     for i in result['hits']['hits']:
         res.append(i['_source'])
     return res
-=======
->>>>>>> dd7243895084e750c9ccaa52cb11233a218fb4a6
 
 @app.route('/results',methods=['GET'])
 def text_Search():
     search = request.args.get('name')
     dsl1 = {'query':{'match':{'name':str(search)}}}
     result = es.search(index='homework1',doc_type='Movie',body=dsl1)
-<<<<<<< HEAD
     res = []
     for i in result['hits']['hits']:
         res.append(i['_source'])
@@ -112,34 +108,6 @@ def result1():
     result = es.search(index='homework1',doc_type='Movie',body=dsl4)
     res = result['hits']['hits'][0]['_source']
     return res
-
-=======
-    results = []
-    for i in result['hits']['hits']:
-        results.append(i['_source'])
-    return results
-
-@app.route('/result',methods=['POST','GET'])
-def img_Search():
-    if request.method == 'GET':
-        search = request.args.get('name')
-        dsl2 = {'query':{'term':{'name':str(search)}}}
-        result = es.search(index='homework2',doc_type='Movie',body=dsl2)
-        result1 = result['hits']['hits'][0]['_source']
-        return result1
-
-    if request.method == 'POST':
-        search = request.files['file']
-        search.save('/result/upload' + secure_filename(search.filename))
-        img = cv2.imread('/result/upload' + secure_filename(search.filename))
-        img1 = np.array(img)
-        search = recognize_text_from_posters(img1)
-        dsl3 = {'query':{'match':{'time':str(search)}}}
-        result = es.search(index='homework1',doc_type='Movie',body=dsl3)
-        result2 = result['hits']['hits'][0]['_source']
-        return result2
->>>>>>> dd7243895084e750c9ccaa52cb11233a218fb4a6
-
 
 
 if __name__ == '__main__':
